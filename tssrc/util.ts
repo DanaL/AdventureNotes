ddc89705856fb8c4e49e9946b8c2d55d3f01e2ab
Dana Larose <ywg.dana@gmail.com>
2022-01-02 10:12:55 -0600

    Beginnings of markdown parsing

--- tssrc/util.ts
+++ tssrc/util.ts
@@ -1,5 +1,39 @@
+enum ParseState {
+	Text,
+	Italics
+}
+
 function toHTML(txt: string): string {
-	let html = txt.replace("\n", "<br/>");
+	let state =  ParseState.Text;
+	let html = "";
+	let subString = "";
+
+	for (let j = 0; j < txt.length; j++) {
+		switch (state) {
+			case ParseState.Text:
+				if (txt[j] == '\n')
+					html += "<br/>";
+				else if (txt[j] == '_')
+					state = ParseState.Italics; 
+				else
+					html += txt[j];
+				break;
+			case ParseState.Italics:
+				if (txt[j] == '_') {
+					html += `<em>${subString}</em>`;
+					subString = "";
+					state = ParseState.Text;
+				}
+				else {
+					subString += txt[j];
+				}
+				break;
+		}
+	}
+
+	if (subString.length > 0)
+		html += subString;
+
 	return html;
 }
 
